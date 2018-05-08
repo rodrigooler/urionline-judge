3fc9d8cf17ab25681da187ee79e5416cfa00aecf
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2018-05-08 04:01:37 -0300

    create 1001

---
+++ 1001/index.js
@@ -0,0 +1,7 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+
+const a = parseInt(lines.shift());
+const b = parseInt(lines.shift());
+
+console.log('X = ' + (a + b)); 
